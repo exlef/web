a820b23cd3dbe34f37989aba695b5a5fe428ae53
Amed <amed.duman@voodoo.io>
2025-03-16 23:42:08 +0300

    js files

--- learn/dist/js/index.js
+++ learn/dist/js/index.js
@@ -15,3 +15,4 @@ let result = (0, ts_pattern_1.match)(tryAdd(3, 3))
     console.log("Addition failed:", value);
 })
     .exhaustive(); // Ensures all cases are handled
+// next : https://www.youtube.com/watch?v=9dw2ik9N8wo&list=PL0Zuz27SZ-6NS8GXt5nPrcYpust89zq_b&index=3
